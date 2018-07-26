a31f3b130751e4ff4c99b2bc3c2c301ac732979a
Takuya Osada <takuya-osada@cookpad.com>
2018-07-26 22:09:12 +0900

    Add 2018 summer

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,10 @@
 
 cd $(dirname $0)/..
 rm -rf build
-mkdir build
+mkdir -p build/archives/0
 cp -r lp/img build/img
 cp -r lp/css build/css
+bundle exec haml lp/archives/0/index.html.haml build/archives/0/index.html
 bundle exec haml lp/index.html.haml build/index.html
 
 if [ "$1" == "deploy" ]; then
